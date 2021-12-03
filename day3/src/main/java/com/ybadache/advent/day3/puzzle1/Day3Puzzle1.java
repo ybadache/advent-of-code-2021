e3ee008aa5a4ac150d310243a637c68ca17bac34
ybadache <yassine.badache@gmail.com>
2021-12-03 22:47:14 +0100

    Cleaning Day 3 Puzzle 1
    From 27 to 17 Cyclo. Complexity

--- day3/src/main/java/com/ybadache/advent/day3/puzzle1/Day3Puzzle1.java
+++ day3/src/main/java/com/ybadache/advent/day3/puzzle1/Day3Puzzle1.java
@@ -2,7 +2,6 @@ package com.ybadache.advent.day3.puzzle1;
 
 import com.ybadache.advent.commons.utils.ObjectCheckingUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Day3Puzzle1 {
@@ -12,42 +11,35 @@ public class Day3Puzzle1 {
             return 0;
         }
 
-        int totalInputSize = input.size();
-        String gammaRate = "";
-        String epsilonRate = "";
+        final int totalInputSize = input.size();
+        final int inputLength = input.get(0).length();
+        final StringBuilder gammaRate = new StringBuilder();
+        final StringBuilder epsilonRate = new StringBuilder();
 
         // There surely is a better way. God forgive what I'll write now
-        List<char[]> splittedBinaryInput = new ArrayList<>();
-        for (String line : input)   {
-            splittedBinaryInput.add(line.toCharArray());
-        }
+        // Update: there was a better way, of course
 
 
         // All input are considered of equal size so no risks there
-        for (int i = 0; i < splittedBinaryInput.get(0).length; i++)     {
-            int numberOfOnes = 0;
-            int numberOfZeros = 0;
-            for (char[] splittedBinary : splittedBinaryInput)   {
-                if (splittedBinary[i] == '1')   {
-                    numberOfOnes++;
-                } else  {
-                    numberOfZeros++;
-                }
+        for (int i = 0; i < inputLength; i++) {
+            // to use into stream().filter()
+            int characterIndex = i;
+            long numberOfOnes = input.stream()
+                    .filter(line -> line.charAt(characterIndex) == '1')
+                    .count();
+            long numberOfZeros = input.stream()
+                    .filter(line -> line.charAt(characterIndex) == '0')
+                    .count();
 
-                if (numberOfOnes > totalInputSize / 2)  {
-                    gammaRate = gammaRate + "1";
-                    epsilonRate = epsilonRate + "0";
-                    break;
-                } else if (numberOfZeros > totalInputSize / 2)  {
-                    gammaRate = gammaRate + "0";
-                    epsilonRate = epsilonRate + "1";
-                    break;
-                }
+            if (numberOfOnes > totalInputSize / 2) {
+                gammaRate.append("1");
+                epsilonRate.append("0");
+            } else if (numberOfZeros > totalInputSize / 2) {
+                gammaRate.append("0");
+                epsilonRate.append("1");
             }
         }
 
-        gammaRate = gammaRate == "" ? "0" : gammaRate;
-        epsilonRate = epsilonRate == "" ? "0" : epsilonRate;
-        return Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2);
+        return Integer.parseInt(gammaRate.toString(), 2) * Integer.parseInt(epsilonRate.toString(), 2);
     }
 }
