7cb2abb968fbb542ce144e9f702d2cc75dd3b67f
ybadache <yassine.badache@gmail.com>
2021-12-04 15:21:11 +0100

    Day 4 : extracting bingo cards

--- day4/src/main/java/com/ybadache/advent/day4/puzzle1/Day4Puzzle1.java
+++ day4/src/main/java/com/ybadache/advent/day4/puzzle1/Day4Puzzle1.java
@@ -1,4 +1,68 @@
 package com.ybadache.advent.day4.puzzle1;
 
+import com.ybadache.advent.commons.utils.ObjectCheckingUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Day4Puzzle1 {
+
+    private final static int BINGO_CARD_SIZE = 5;
+    public int resolve(List<String> input)      {
+        if (ObjectCheckingUtils.isListNullOrEmpty(input) )     {
+            return 0;
+        }
+
+        final List<String> inputCopy = new ArrayList<>();
+        inputCopy.addAll(input);
+
+        // The first line contains all the inputs for the bingo cards
+        List<String> bingoDraws = Arrays.asList(inputCopy.get(0).split(","));
+        inputCopy.remove(0);
+        // The second line is a chariot and we don't need it to fill our arrays
+        inputCopy.remove(0);
+
+
+        List<String[][]> allBingoCards = this.extractBingoCardsFromInput(inputCopy);
+        List<boolean[][]> allBingoCardTags = this.initializeBingoCardsTags(allBingoCards.size());
+
+        return 0;
+    }
+
+    public List<String[][]> extractBingoCardsFromInput(List<String> input)  {
+        int lineIndex = 0;
+        List<String[][]> allBingoCards = new ArrayList<>();
+
+
+        // We know that a bingo card is 5 x 5
+        String[][] bingoCard = new String[BINGO_CARD_SIZE][BINGO_CARD_SIZE];
+        for (String line : input)   {
+            if ("\n".equals(line))  {
+                lineIndex = 0;
+                allBingoCards.add(bingoCard.clone());
+                bingoCard = new String[BINGO_CARD_SIZE][BINGO_CARD_SIZE];
+            } else {
+                String[] bingoLine = line.split("\\s+");
+                for (int i = 0; i < bingoLine.length; i++)  {
+                    bingoCard[lineIndex][i] = bingoLine[i];
+                }
+
+                lineIndex++;
+            }
+        }
+
+        return allBingoCards;
+    }
+
+    public List<boolean[][]> initializeBingoCardsTags(int numberOfBingos)   {
+        List<boolean[][]> allBingoCardsTags = new ArrayList<>();
+        for (int i = 0; i < numberOfBingos; i++)     {
+            final boolean[][] bingoCardTags = new boolean[][]{};
+            allBingoCardsTags.add(bingoCardTags.clone());
+        }
+
+        return allBingoCardsTags;
+    }
 }
--- day4/src/test/java/com/ybadache/advent/day4/puzzle1/Day4Puzzle1Test.java
+++ day4/src/test/java/com/ybadache/advent/day4/puzzle1/Day4Puzzle1Test.java
@@ -1,4 +1,98 @@
 package com.ybadache.advent.day4.puzzle1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Day4Puzzle1Test {
+
+    @Test
+    public void extractBingoCardsFromInputTest()    {
+        final Day4Puzzle1 day4Puzzle1 = new Day4Puzzle1();
+
+        // Compared to the initial input, we removed the first two lines
+        final List<String> input = Arrays.asList(
+                "22 13 17 11  0",
+                "8  2 23  4 24",
+                "21  9 14 16  7",
+                "6 10  3 18  5",
+                "1 12 20 15 19",
+                "\n",
+                "3 15  0  2 22",
+                "9 18 13 17  5",
+                "19  8  7 25 23",
+                "20 11 10 24  4",
+                "14 21 16 12  6",
+                "\n",
+                "14 21 17 24  4",
+                "10 16 15  9 19",
+                "18  8 23 26 20",
+                "22 11 13  6  5",
+                "2  0 12  3  7",
+                "\n"
+        );
+
+        List<String[][]> extractedBingoCards = day4Puzzle1.extractBingoCardsFromInput(input);
+        Assertions.assertNotNull(extractedBingoCards);
+        Assertions.assertNotEquals(0, extractedBingoCards.size());
+
+        for (String[][] bingoCard : extractedBingoCards)    {
+            for (int i = 0; i < bingoCard.length - 1; i++)  {
+                for (int j = 0; j < bingoCard[i].length - 1; j++)   {
+                    Assertions.assertNotNull(bingoCard[i][j]);
+                    Assertions.assertNotEquals("", bingoCard[i][j]);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void initializeBingoCardsTagsTest() {
+        final Day4Puzzle1 day4Puzzle1 = new Day4Puzzle1();
+        final List<boolean[][]> initializedBingoCardsTags = day4Puzzle1.initializeBingoCardsTags(5);
+
+        Assertions.assertNotNull(initializedBingoCardsTags);
+        Assertions.assertNotEquals(0, initializedBingoCardsTags.size());
+        Assertions.assertEquals(5, initializedBingoCardsTags.size());
+
+        for (boolean[][] bingoCardTag : initializedBingoCardsTags)  {
+            for (int i = 0; i < bingoCardTag.length - 1; i++)  {
+                for (int j = 0; j < bingoCardTag[i].length - 1; j++)   {
+                    Assertions.assertFalse(bingoCardTag[i][j]);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void day4puzzle1SampleTest()     {
+        final Day4Puzzle1 day4Puzzle1 = new Day4Puzzle1();
+        final List<String> input = Arrays.asList(
+                "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+                "\n",
+                "22 13 17 11  0",
+                "8  2 23  4 24",
+                "21  9 14 16  7",
+                "6 10  3 18  5",
+                "1 12 20 15 19",
+                "\n",
+                "3 15  0  2 22",
+                "9 18 13 17  5",
+                "19  8  7 25 23",
+                "20 11 10 24  4",
+                "14 21 16 12  6",
+                "\n",
+                "14 21 17 24  4",
+                "10 16 15  9 19",
+                "18  8 23 26 20",
+                "22 11 13  6  5",
+                "2  0 12  3  7",
+                "\n"
+        );
+
+        final int expectedAnswer = day4Puzzle1.resolve(input);
+        Assertions.assertEquals(4512, expectedAnswer);
+    }
 }
